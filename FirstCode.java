761a8658240415488116aa0ae3282cd2e2a4675f
Agrawal-Kshitij <kshitijagrawal234@gmail.com>
2025-07-13 16:37:53 +0530

    Code initialisation

---
+++ FirstCode.java
@@ -0,0 +1,7 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class FirstCode {
+
+   public static void main(String[] var0) {
+      System.out.println("Hii");
+   }
+}
