c9d29f43309e8037f4530e63267dec8d9907322c
Agrawal-Kshitij <kshitijagrawal234@gmail.com>
2025-07-13 16:51:25 +0530

    Initialisation of Graph

--- FirstCode.java
+++ FirstCode.java
@@ -1,7 +1,20 @@
 // Source code is decompiled from a .class file using FernFlower decompiler.
+
+import java.util.HashMap;
+
 public class FirstCode {
+    public static class Graph_M{
+        public static class Vertex{
+            HashMap<String , Integer> nbrs = new HashMap<>();
+        }
+        static HashMap<String, Vertex> MetroMap;
+        public Graph_M(){
+            MetroMap = new HashMap<>();
+        }
+    }
 
    public static void main(String[] var0) {
       System.out.println("Hii");
+      Graph_M g = new Graph_M();
    }
 }
